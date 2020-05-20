a9c41613dc3df17e2230fa70f0d199c73b12487f
Carlos Fdez. Llamas <hello@sirikon.me>
2020-05-21 00:14:13 +0200

    reformat using tabs

--- src/java-management.sh
+++ src/java-management.sh
@@ -6,50 +6,50 @@ stateFolder="${MC_ROOT}/state"
 currentJavaVersionfile="${stateFolder}/current-java-version"
 
 function switch-java-command {
-    javaVersion="${1:-""}"
-    currentJavaVersion=$(get-current-java-version)
+	javaVersion="${1:-""}"
+	currentJavaVersion=$(get-current-java-version)
 
-    if [ "$javaVersion" == "" ]; then
-        printf "Choose an available version:\n"
-        cat $javaVersionsPath | while read -r version link
-        do
-            check=" "
-            if [ "$currentJavaVersion" == "$version" ]; then
-                check="x"
-            fi
-            printf "[$check] $version\n"
-        done
-        printf "\nSwitch to a new one with 'ourcraft switch-java <version>'\n"
-    else
-        ensure-java-downloaded "$javaVersion"
-        set-current-java-version "$javaVersion"
-    fi
+	if [ "$javaVersion" == "" ]; then
+		printf "Choose an available version:\n"
+		cat $javaVersionsPath | while read -r version link
+		do
+			check=" "
+			if [ "$currentJavaVersion" == "$version" ]; then
+				check="x"
+			fi
+			printf "[$check] $version\n"
+		done
+		printf "\nSwitch to a new one with 'ourcraft switch-java <version>'\n"
+	else
+		ensure-java-downloaded "$javaVersion"
+		set-current-java-version "$javaVersion"
+	fi
 }
 
 function ensure-java-downloaded {
-    javaVersion="$1"
-    javaVersionLink=$(get-java-version-link "$javaVersion")
-    javaFolder="${MC_ROOT}/java/${javaVersion}"
-    if [ ! -d "$javaFolder" ]; then
-        mkdir -p "$javaFolder"
-        curl -LQ --progress-bar "$javaVersionLink" --output "$javaFolder/java.tar.gz"
-        (
-            cd "$javaFolder"
-            tar --strip-components=1 -xzf java.tar.gz
-            rm java.tar.gz
-        )
-    fi
+	javaVersion="$1"
+	javaVersionLink=$(get-java-version-link "$javaVersion")
+	javaFolder="${MC_ROOT}/java/${javaVersion}"
+	if [ ! -d "$javaFolder" ]; then
+		mkdir -p "$javaFolder"
+		curl -LQ --progress-bar "$javaVersionLink" --output "$javaFolder/java.tar.gz"
+		(
+			cd "$javaFolder"
+			tar --strip-components=1 -xzf java.tar.gz
+			rm java.tar.gz
+		)
+	fi
 }
 
 function get-java-version-link {
-    javaVersion="$1"
-    awk -v javaVersion="$javaVersion" '{if($1 == javaVersion){print $2}}' src/assets/java-versions | head -n 1
+	javaVersion="$1"
+	awk -v javaVersion="$javaVersion" '{if($1 == javaVersion){print $2}}' src/assets/java-versions | head -n 1
 }
 
 function set-current-java-version {
-    javaVersion="$1"
-    mkdir -p "${stateFolder}"
-    printf "$javaVersion" > "${stateFolder}/current-java-version"
+	javaVersion="$1"
+	mkdir -p "${stateFolder}"
+	printf "$javaVersion" > "${stateFolder}/current-java-version"
 }
 
 function get-current-java-version {
@@ -57,7 +57,3 @@ function get-current-java-version {
 		cat "${stateFolder}/current-java-version"
 	fi
 }
-
-
-
-
--- src/main.sh
+++ src/main.sh
@@ -9,46 +9,46 @@ source "${MC_ROOT}/src/java-management.sh"
 source "${MC_ROOT}/src/service-management.sh"
 
 function main {
-    command="${1:-"help"}"
-    args="${@:2}"
-    case "$command" in
-        start) start-command ;;
-        switch-java) switch-java-command "$args" ;;
-        create-service) create-service-command ;;
-        help) help-command ;;
-        *) unknown-command "$command" ;;
-    esac
+	command="${1:-"help"}"
+	args="${@:2}"
+	case "$command" in
+		start) start-command ;;
+		switch-java) switch-java-command "$args" ;;
+		create-service) create-service-command ;;
+		help) help-command ;;
+		*) unknown-command "$command" ;;
+	esac
 }
 
 function start-command {
-    JAVA_VERSION="$(get-current-java-version)"
-    (
-        export JAVA_HOME="${MC_ROOT}/java/${JAVA_VERSION}"
-        export PATH="$PATH:${JAVA_HOME}/bin"
-    	cd "${MC_ROOT}/server"
-    	java \
-                    -Xmx${MEMORY} -Xms${MEMORY} \
-		    -XX:+UnlockExperimentalVMOptions \
-                    -XX:+AlwaysPreTouch \
-                    -XX:+UseG1GC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 \
-                    -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 \
-                    -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 \
-                    -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 \
-                    -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC \
-                    -XX:InitiatingHeapOccupancyPercent=15 \
-                    -XX:G1MixedGCLiveThresholdPercent=90 \
-                    -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
-                    -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
-                    -jar $SERVER_JAR
-    )
+	JAVA_VERSION="$(get-current-java-version)"
+	(
+		export JAVA_HOME="${MC_ROOT}/java/${JAVA_VERSION}"
+		export PATH="$PATH:${JAVA_HOME}/bin"
+		cd "${MC_ROOT}/server"
+		java \
+			-Xmx${MEMORY} -Xms${MEMORY} \
+			-XX:+UnlockExperimentalVMOptions \
+			-XX:+AlwaysPreTouch \
+			-XX:+UseG1GC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 \
+			-XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 \
+			-XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 \
+			-XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 \
+			-XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC \
+			-XX:InitiatingHeapOccupancyPercent=15 \
+			-XX:G1MixedGCLiveThresholdPercent=90 \
+			-XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
+			-XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
+			-jar $SERVER_JAR
+	)
 }
 
 function help-command {
-    printf "You're alone, buddy.\n"
+	printf "You're alone, buddy.\n"
 }
 
 function unknown-command {
-    printf "Unknown command '%s'\n" "$1"
+	printf "Unknown command '%s'\n" "$1"
 }
 
 main $@

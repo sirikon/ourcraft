0a6fc6943557075dc9920faebce94b5c0ed1543c
Carlos Fdez. Llamas <hello@sirikon.me>
2020-07-18 18:44:14 +0200

    Waiter

--- src/daemons/minecraft_runner.cr
+++ src/daemons/minecraft_runner.cr
@@ -3,20 +3,25 @@ module Ourcraft::Daemons::MinecraftRunner
 
   class MinecraftProcess
     property proc : Process?
+    property waiters : Array(Channel(Nil)) = [] of Channel(Nil)
 
     def start
       if @proc != nil
         return
       end
+
       @proc = Process.new(
         command: "ping",
         env: {
           "PATH" => ENV["PATH"],
         },
         args: ["8.8.8.8"],
+        input: STDIN,
         output: STDOUT,
         error: STDERR,
         chdir: ".")
+
+      handle_process_termination
     end
 
     def stop
@@ -24,8 +29,24 @@ module Ourcraft::Daemons::MinecraftRunner
         return
       end
       @proc.not_nil!.signal(Signal::INT)
-      @proc.not_nil!.wait
-      @proc = nil
+      wait
+    end
+
+    private def handle_process_termination
+      spawn do
+        @proc.not_nil!.wait
+        @waiters.each do |chan|
+          chan.send(nil)
+        end
+        @waiters.clear
+        @proc = nil
+      end
+    end
+
+    private def wait
+      chan = Channel(Nil).new
+      @waiters << chan
+      chan.receive
     end
   end
 
